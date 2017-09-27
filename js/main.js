7cbcd47ab9b14734b86c11a2ec64bf0bdce9f822
Ben Murray <ben@nextrealm.co.uk>
2017-09-27 16:52:25 +0100

    Initial commit.
    Adding the base files.

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+	var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-canvas', { preload: preload, create: create });
+
+	function preload () {
+		game.load.image('logo', 'assets/comp-soc.png');
+	}
+
+	function create () {
+		var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+		logo.anchor.setTo(0.5, 0.5);
+	}
+});
