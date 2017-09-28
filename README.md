875b271d290dabeb7911d029e0604d9e8e38c2d9
Ben Murray <nextrealm@users.noreply.github.com>
2017-09-28 10:18:28 +0100

    Added text.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # HTML_Template
 A HTML game starting point for those wanting to learn.
+
+Built using the [Phaser.io] (https://phaser.io/) HTML5 game framework.
--- js/main.js
+++ js/main.js
@@ -8,5 +8,16 @@ $(function(){
 	function create () {
 		var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
 		logo.anchor.setTo(0.5, 0.5);
+		
+		var style = {
+		font: "16px Arial",
+		fill: "#fff", 
+        align: "left",
+        boundsAlignH: "left", 
+        boundsAlignV: "top", 
+        wordWrap: true,
+        wordWrapWidth: 300
+        };
+        text = game.add.text(0, 0, "University of Derby/nComputing Society", style);
 	}
 });
